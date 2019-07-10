12741bd82cad150359b3355fa7cc9269f7ebbec9
Alexandra Zhang Jiang <52464618+Ale-ZJ@users.noreply.github.com>
2019-07-10 15:19:21 -0700

    Add CSS and JavaScript for the GWC project web page

---
+++ index.js
@@ -0,0 +1,18 @@
+// window.alert("Welcome to my page about One Piece!");
+console.log("Hello World")
+
+function getLuffy(){
+  document.getElementById("luffy").src='luffy_run.gif';
+}
+
+function getLuffy_attack(){
+  document.getElementById("luffy").src='luffy_punch_gauntlet.gif';
+}
+
+function getLuffy_eat(){
+  document.getElementById("luffy").src='luffy_eat.gif';
+}
+
+function getLuffy_roll(){
+  document.getElementById("luffy").src='luffy_rolling.gif';
+}
